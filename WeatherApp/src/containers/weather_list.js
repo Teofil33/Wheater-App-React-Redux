e4e17731fa4df6e579c71935c27e0488f161e017
Bojan <bojanj6m@gmail.com>
2017-02-28 16:20:23 -0500

    Chart, Weather List

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -9,6 +9,7 @@ import Chart from '../components/chart'
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name  = cityData.city.name
+    //const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273 );
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
@@ -18,13 +19,13 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="K" />
         </td>
         <td>
-          <Chart data={pressures} color="green" />
+          <Chart data={pressures} color="green" units="hPa" />
         </td>
         <td>
-          <Chart data={humidities} color="black" />
+          <Chart data={humidities} color="black" units="%" />
         </td>
       </tr>
     );
@@ -48,9 +49,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
