6a05c9a9e54a450a750a01fdce10a54fc60defa4
Bojan <bojanj6m@gmail.com>
2017-02-28 16:53:30 -0500

    Google Maps Integration

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { Sparklines, SparklinesLine } from 'react-sparklines';
-import Chart from '../components/chart'
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 // React sparkline https://github.com/borisyankov/react-sparklines
 // npm install --save react-sparklines
@@ -13,11 +14,14 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
+    // const lat = cityData.city.coord.lat;
+    // const lon = cityData.city.coord.lon;
     console.log(temps);
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td>
           <Chart data={temps} color="orange" units="K" />
         </td>
